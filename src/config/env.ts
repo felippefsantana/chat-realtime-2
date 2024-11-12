abe5c6867851326f532003bccd553872d10c1ed7
Felippe Santana <felippefsantana02@gmail.com>
2024-11-12 12:32:05 -0300

    adjust docker compose and config of redis

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,15 +1,18 @@
 import dotenv from "dotenv";
+import dotenvExpand from "dotenv-expand";
 import path from "node:path";
 import { z } from "zod";
 
 const envPath = path.resolve(__dirname, "../../.env");
 
-dotenv.config({
+const envs = dotenv.config({
   path: envPath,
 });
+dotenvExpand.expand(envs);
 
 const envSchema = z.object({
   NODE_ENV: z.string(),
+  APP_NAME: z.string(),
   APP_HOST: z.string(),
   APP_PORT: z.coerce.number(),
   APP_URL: z.string().url(),
--- src/config/redis.ts
+++ src/config/redis.ts
@@ -2,5 +2,5 @@ import { env } from "./env";
 
 export default {
   host: env.REDIS_HOST,
-  port: env.REDIS_PORT || 6379,
+  port: env.REDIS_PORT,
 };
